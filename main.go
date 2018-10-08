f13a7dd771fba412f070618d05fa94c6e1b1acee
sadewoeko <sadewoeko18@gmail.com>
2018-10-08 20:59:44 +0700

    private pertama

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 // function yang memberikan return
 func getUser() string {
-	a := "Mami Euis"
+	a := "Mami Euis\n"
 	return a
 }
 
@@ -15,8 +15,54 @@ func getAlamat() {
 	fmt.Println("Pondok Bambu")
 }
 
+func getHobby(){
+	fmt.Println("openBO")
+}
+
+func getGaya(){
+	fmt.Println("maco bangetz")
+}
+
+func getCitacita(){
+	fmt.Println("punya cewe banyak")
+}
+
+func getUnivercity(){
+	fmt.Println("unsada dong")
+}
+
+func getNamadepan() string{
+	b := "eko"
+	return b
+}
+
+func getNamatengah() string{
+	c := "bambang"
+	return c
+}
+
+func getNamabelakang() string{
+	d := "sadewo"
+	return d
+}
+
+func getUmur() int{
+	e := 23
+	return e
+}
+
+
 func main() {
 	var nama = getUser()
-	fmt.Println(nama)
+	var depan = getNamadepan()
+	var tengah = getNamatengah()
+	var belakang = getNamabelakang()
+	var umur = getUmur()
+	fmt.Println(nama, depan, tengah, belakang, umur)
+	
 	getAlamat()
+	getHobby()
+	getGaya()
+	getCitacita()
+	getUnivercity()
 }
