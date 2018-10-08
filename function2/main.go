93d90c0e4f54e07c3e1307a8cd332d1822be4104
sadewoeko <sadewoeko18@gmail.com>
2018-10-09 00:47:59 +0700

    percobaan function 3

--- function2/main.go
+++ function2/main.go
@@ -12,7 +12,38 @@ func getUmur(user string) int {
 	}
 }
 
+func getGender(gender string) string{
+	if gender == "Pram"{
+		return "men" 
+	} else{
+		return "girl"
+	}
+}
+
+func getUnivyear(year int) string{
+	if year == 2013{
+		return "swasta"
+	} else {
+		return "negri"
+	}
+}
+
+func getExwoman(women string) string{
+	if women == "quena"{
+		return "paling cantik"
+	} else if women == "maharani"{
+		return "bogel cantik"
+	} else if women == "simawar" {
+		return "biasa ngga cantik banget"
+	} else {
+		return "baik & muslimah"
+	}
+}
+
 func main() {
 	umur := getUmur("Kukuh")
-	fmt.Println(umur)
+	gender := getGender("Pram")
+	year := getUnivyear(2013)
+	women := getExwoman("simawar")
+	fmt.Println(umur, gender, year, women)
 }
---
+++ function3/main.go
@@ -0,0 +1,65 @@
+package main
+
+import(
+	"fmt"
+)
+
+func underscore(){
+	name := new(string)
+	fmt.Println(name)
+}
+
+func number(){
+	var positiveNumber uint8 = 100
+	var negativeNumber = -1243423644
+
+	fmt.Printf("bilangan positif: %d\n", positiveNumber)
+	fmt.Printf("bilangan negatif: %d\n", negativeNumber)
+}
+
+func decimal(){
+	var decimalNumber = 3.35
+
+	fmt.Printf("bilangan decimal: %f\n", decimalNumber)
+	fmt.Printf("bilangan decimal kelipatan 3: %.3f\n", decimalNumber)
+	fmt.Printf("bilangan decimal kelimatan 5: %.5f\n", decimalNumber)
+	fmt.Printf("bilangan decimal kelimatan 10: %.10f\n", decimalNumber)
+}
+
+func booelan(){
+	var exist bool = true
+	fmt.Printf("exist? %t \n", exist)
+}
+
+func boolgede(){
+	var idontcare bool = false
+	fmt.Printf("idontcare? %t \n", idontcare)
+}
+
+func tipedata(){
+	var message string = "Halo"
+	fmt.Printf("message: %s \n", message)
+}
+
+func pesan(){
+	var message = `Nama saya "echo".
+	Salam kenal.
+	yang belum bisa membahagiakan mantan.`
+
+	fmt.Println(message)
+}
+
+
+func main(){
+	var first, secound, third = "eko", "bambang", "sadewo"
+	_ = "ilang pasti\n"
+
+	fmt.Println(first, secound, third)
+	underscore()
+	number()
+	decimal()
+	booelan()
+	boolgede()
+	tipedata()
+	pesan()
+}
